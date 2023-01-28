4848215bb58deb26a321cf49d3aa26919f30e167
kingadamo <78397077+kingadamo@users.noreply.github.com>
2023-01-28 13:02:11 -0500

    Initial commit

---
+++ src/main/java/com/ecolify/connecthub/ConnecthubApplication.java
@@ -0,0 +1,13 @@
+package com.ecolify.connecthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnecthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnecthubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecolify/connecthub/ConnecthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecolify.connecthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnecthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
