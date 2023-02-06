e50d87e5a939fcb4dcdaf82f125352ad1633451d
kingadamo <78397077+kingadamo@users.noreply.github.com>
2023-02-06 15:16:35 -0500

    Update ConnecthubApplicationTests.java

--- src/test/java/com/ecolify/connecthub/ConnecthubApplicationTests.java
+++ src/test/java/com/ecolify/connecthub/ConnecthubApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ConnecthubApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
